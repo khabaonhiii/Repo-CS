5923ba66679a5bf78e604665edc78ebb4f6f7b31
Kha Bao Nhi <110595210+khabaonhiii@users.noreply.github.com>
2023-04-25 11:52:54 +0700

    drag and drop a test step

--- test-cases/Login to the demo website.ks.ts
+++ test-cases/Login to the demo website.ks.ts
@@ -3,6 +3,39 @@ import katalon from "katalon";
 export default katalon.testCase("New Test Case", async ({ local, web }) => {
   await new Promise((r) => setTimeout(r, 0));
   await web.openBrowser("https://katalon-demo-cura.herokuapp.com");
+  await web.click({
+    type: "Web",
+    id: "5e9b0ecd-809f-4e44-a4e0-c7b7e905a8d6",
+    attributes: {
+      type: "password",
+      class: "form-control",
+      id: "txt-password",
+      name: "password",
+      placeholder: "Password",
+      value: "",
+      autocomplete: "off",
+    },
+    childIndex: 1,
+    hashes: {},
+    name: "input - Password",
+    selectors: [
+      {
+        id: "4c1107e4-4b5b-4d79-987b-67daa4268120",
+        type: "CSS",
+        value: "#txt-password",
+        isDefault: true,
+      },
+      {
+        id: "2e7d2159-13ce-4bb0-bbde-7f659b211638",
+        type: "Attribute",
+        isDefault: true,
+      },
+    ],
+    selectorType: "CSS",
+    tag: "input",
+    text: "",
+    pageUrl: "https://katalon-demo-cura.herokuapp.com/profile.php#login",
+  });
   await web.setText(
     {
       type: "Web",
@@ -39,37 +72,4 @@ export default katalon.testCase("New Test Case", async ({ local, web }) => {
     },
     "John Doe"
   );
-  await web.click({
-    type: "Web",
-    id: "5e9b0ecd-809f-4e44-a4e0-c7b7e905a8d6",
-    attributes: {
-      type: "password",
-      class: "form-control",
-      id: "txt-password",
-      name: "password",
-      placeholder: "Password",
-      value: "",
-      autocomplete: "off",
-    },
-    childIndex: 1,
-    hashes: {},
-    name: "input - Password",
-    selectors: [
-      {
-        id: "4c1107e4-4b5b-4d79-987b-67daa4268120",
-        type: "CSS",
-        value: "#txt-password",
-        isDefault: true,
-      },
-      {
-        id: "2e7d2159-13ce-4bb0-bbde-7f659b211638",
-        type: "Attribute",
-        isDefault: true,
-      },
-    ],
-    selectorType: "CSS",
-    tag: "input",
-    text: "",
-    pageUrl: "https://katalon-demo-cura.herokuapp.com/profile.php#login",
-  });
 });
